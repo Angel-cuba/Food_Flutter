cea987ede8b85247ecf522ab8235497b99893d2f
Mac_Developer <araozangel842@gmail.com>
2023-01-26 00:15:00 +0200

    Fetch dummy data

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/widgets/recipe_card.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -10,19 +11,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,6 +24,12 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      body: RecipeCard(
+          title: 'Tacos',
+          cookTime: '12 minuttes',
+          rating: '3',
+          thumbnailUrl:
+              'https://res.cloudinary.com/dqaerysgb/image/upload/v1635360680/mexico_h9dcjv.jpg'),
     );
   }
 }
